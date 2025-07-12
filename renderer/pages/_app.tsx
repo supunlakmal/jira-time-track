414fe27e395500379d99ecb3658a9aad71483301
Supun Lakmal <50650071+supun156@users.noreply.github.com>
2025-07-12 09:25:26 +0530

    update scss

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -4,7 +4,9 @@ import { Provider } from "react-redux";
 import { store } from "../store/store";
 import { ThemeProvider } from "../components/theme/ThemeProvider";
 
-import "../styles/globals.css";
+// import "../styles/globals.css";
+import "../styles/assets/scss/app.scss";
+import "../styles/assets/scss/icons.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -1,26 +1,42 @@
-const colors = require('tailwindcss/colors')
+// const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/components/**/*.{js,ts,jsx,tsx}',
-    '../stories/**/*.{js,ts,jsx,tsx}',
+    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
+    "./renderer/components/**/*.{js,ts,jsx,tsx}",
+    "../stories/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: ["class", '[data-mode="dark"]'],
   theme: {
-    colors: {
-      // use colors only specified
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      blue: colors.blue,
-      green: colors.green,
-      red: colors.red,
-      yellow: colors.yellow,
-      orange: colors.orange,
-      purple: colors.purple,
+    container: {
+      center: true,
     },
-    extend: {},
+    fontFamily: {
+      base: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#3073F1",
+        secondary: "#68625D",
+        success: "#1CB454",
+        warning: "#E2A907",
+        info: "#0895D8",
+        danger: "#E63535",
+        light: "#eef2f7",
+        dark: "#313a46",
+      },
+      keyframes: {
+        load: {
+          "0%": { width: "0%" },
+          "100%": { width: "100%" },
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@frostui/tailwindcss/plugin"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
